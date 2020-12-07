f498167d611d9025a4a390db244b91c20baa9cd5
zouhaier barrouta <zouhaier.cr@gmail.com>
2020-12-07 23:05:45 +0100

    first commit

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   database:
     'mongodb://localhost:27017/ShowMovies',
   port: 5000,
-  secret: 'ArashYahaya2012312321'
+  secret: '123'
 };
--- routes/main.js
+++ routes/main.js
@@ -2,106 +2,136 @@ const router = require('express').Router();
 const Category = require('../models/category');
 const Product = require('../models/product');
 const Review = require('../models/review');
+const News = require('../models/news');
 
 
 
 const async = require('async');
 const chekJWT = require('../middlewares/check-jwt')
 
-router.get('/products', (req, res, next)=>{
+router.get('/products', (req, res, next) => {
     const perPage = 10;
-    const page =req.query.page;
+    const page = req.query.page;
     async.parallel([
-        function(callback){
-            Product.count({}, (err, count)=>{
-                var totalProducts = count;
-                callback(err, totalProducts);
-            });
-        },
-        function(callback){
-            Product.find({})
-                .skip(page*perPage)
-                .limit(perPage)
-                .populate('category')
-                .populate('owner')
-                .exec((err, products)=>{
-                    if(err) return next(err);
-                    callback(err, products);
-                });
-        }
-    ], function(err,results){
-        var totalProducts = results[0];
-        var products = results[1];
-        res.json({
-            success : true,
-            message : 'products',
-            products : products,
-            totalProducts : totalProducts,
-            pages : Math.ceil(totalProducts / perPage)
+      function(callback) {
+        Product.count({}, (err, count) => {
+          var totalProducts = count;
+          callback(err, totalProducts);
         });
+      },
+      function(callback) {
+        Product.find({})
+          .skip(perPage * page)
+          .limit(perPage)
+          .populate('category')
+          .populate('owner')
+          .exec((err, products) => {
+            if(err) return next(err);
+            callback(err, products);
+          });
+      }
+    ], function(err, results) {
+      var totalProducts = results[0];
+      var products = results[1];
+     
+      res.json({
+        success: true,
+        message: 'category',
+        products: products,
+        totalProducts: totalProducts,
+        pages: Math.ceil(totalProducts / perPage)
+      });
     });
-});
-
-router.route('/categories')
-    .get((req, res ,next)=>{
-        Category.find({}, (err, categories)=>{
-            res.json({
-                success : true,
-                message : 'success',
-                categories : categories
-            });
-        });
+    
+  });
+  
+  router.route('/categories')
+    .get((req, res, next) => {
+      Category.find({}, (err, categories) => {
+        res.json({
+          success: true,
+          message: "Success",
+          categories: categories
+        })
+      })
     })
-    .post((req, res, next)=>{
-        let category = new Category();
-        category.name = req.body.name;
-        category.save();
-        res.json({
-            success : true,
-            message : 'category added successfully'
-        });
+    .post((req, res, next) => {
+      let category = new Category();
+      category.name = req.body.category;
+      category.save();
+      res.json({
+        success: true,
+        message: "Successful"
+      });
     });
-
-router.get('/categories/:id', (req, res, next)=>{
-    const perPage = 10;
-    const page =req.query.page;
-    async.parallel([
-        function(callback){
-            Product.count({category : req.params.id}, (err, count)=>{
-                var totalProducts = count;
-                callback(err, totalProducts);
+  
+  
+    router.get('/categories/:id', (req, res, next) => {
+      const perPage = 10;
+      const page = req.query.page;
+      async.parallel([
+        function(callback) {
+          Product.count({ category: req.params.id }, (err, count) => {
+            var totalProducts = count;
+            callback(err, totalProducts);
+          });
+        },
+        function(callback) {
+          Product.find({ category: req.params.id })
+            .skip(perPage * page)
+            .limit(perPage)
+            .populate('category')
+            .populate('owner')
+            .populate('reviews')
+            .exec((err, products) => {
+              if(err) return next(err);
+              callback(err, products);
             });
         },
-        function(callback){
-            Product.find({category : req.params.id})
-                .skip(page*perPage)
-                .limit(perPage)
-                .populate('category')
-                .populate('owner')
-                .populate('reviews')
-                .exec((err, products)=>{
-                    if(err) return next(err);
-                    callback(err, products);
-                });
-        },
-        function(callback){
-            Category.findOne({_id : req.params.id}, (err, category)=>{
-                callback(err,category);
-            });
+        function(callback) {
+          Category.findOne({ _id: req.params.id }, (err, category) => {
+           callback(err, category)
+          });
         }
-    ], function(err,results){
+      ], function(err, results) {
         var totalProducts = results[0];
         var products = results[1];
         var category = results[2];
         res.json({
-            success : true,
-            message : 'category',
-            products : products,
-            categoryName : category.name,
-            totalProducts : totalProducts,
-            pages : Math.ceil(totalProducts / perPage)
+          success: true,
+          message: 'category',
+          products: products,
+          categoryName: category.name,
+          totalProducts: totalProducts,
+          pages: Math.ceil(totalProducts / perPage)
+        });
+      });
+      
+    });
+  
+    router.get('/product/:id', (req, res, next) => {
+      Product.findById({ _id: req.params.id })
+        .populate('category')
+        .populate('owner')
+        .deepPopulate('reviews.owner')
+        .exec((err, product) => {
+          if (err) {
+            res.json({
+              success: false,
+              message: 'Product is not found'
+            });
+          } else {
+            if (product) {
+              res.json({
+                success: true,
+                product: product
+              });
+            }
+          }
         });
     });
+  
+  
     /*Product.find({category : req.params.id})
            .populate('category')
            .exec((err, products)=>{
@@ -117,33 +147,9 @@ router.get('/categories/:id', (req, res, next)=>{
                });
            });*/
 
-});
 
-router.get('/product/:id', (req,res,next)=>{
-    Product.findById({_id : req.params.id})
-        .populate('owner')
-        .populate('category')
-        .deepPopulate('reviews.owner')
-        .exec((err, product)=>{
-            if(err){
-                res.json({
-                    success : false,
-                    message : 'Product is not found'
-                });
-            } else {
-                if(product){
-                    res.json({
-                        success : true,
-                        //message : 'product',
-                        product : product
-                    });
-                }
-            }
 
-        });
-});
-
-router.post('/review', chekJWT, (req,res,next)=>{
+router.post('/addreview', chekJWT, (req,res,next)=>{
     async.waterfall([
         function(callback){
             Product.findOne({ _id: req.body.productId }, (err, product)=>{
@@ -171,8 +177,58 @@ router.post('/review', chekJWT, (req,res,next)=>{
         }
     ])
 });
+router.get('/reviews/:id', chekJWT, (req, res, next) => {
+    Review.findOne({_id : req.params.id}, (err, review)=>{
+        console.log(review);
+        review.report ++;
+        review.save();
+        res.json({
+            success : true,
+            review : review
+        });
+    });
+});
 
- 
+
+router.post('/addNews', chekJWT, (req,res,next)=>{
+    async.waterfall([
+        function(callback){
+            Product.findOne({ _id: req.body.productId }, (err, product)=>{
+                if(product){
+                    callback(err, product)
+                }
+            })
+        },
+        function(product){
+            let  news = new News()
+            News.owner = req.decoded.user._id
+
+            if(req.body.title)
+                news.title = req.body.title
+            if(req.body.description)
+                news.description = req.body.description
+                news.rating = req.body.rating
+            product.reviews.push(news._id)
+            product.save()
+            news.save()
+            res.json({
+                success: true,
+                message: 'news was added successfully'
+            })
+        }
+    ])
+});
+router.get('/news/:id', chekJWT, (req, res, next) => {
+    News.findOne({_id : req.params.id}, (err, news)=>{
+        console.log(news);
+        news.report ++;
+        news.save();
+        news.json({
+            success : true,
+            news : news
+        });
+    });
+});
 
     
     
